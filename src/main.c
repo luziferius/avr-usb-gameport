411e3efa5cd9c6f24373b4a433e4e9b88536435c
Thomas Hess <thomas.hess@udo.edu>
2020-05-10 23:01:36 +0200

    Initial project layout

---
+++ src/main.c
@@ -0,0 +1,36 @@
+/* Copyright (C) 2020 Thomas Hess <thomas.hess@udo.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <avr/io.h>
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
+
+#include "usbdrv.h"
+
+int main() {
+    // Disable some unused components: USART, SPI, 16 bit TIMER1
+    PRR |=  _BV(PRUSART0) | _BV(PRSPI) | _BV(PRTIM1);
+    
+
+    for(;;) {
+
+    }
+}
+
+
+
+
